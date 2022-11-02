490d5c9a58aee3cb4fcfd16b8fa01f488e940159
Diogo45 <diogohartmann45@gmail.com>
2022-11-02 14:53:09 -0300

    Added a Filtered Function
    returns correct page defined by page_size and orded by the desired propriety

--- InterviewTestPagination/Controllers/TodoController.cs
+++ InterviewTestPagination/Controllers/TodoController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http;
 using InterviewTestPagination.Models;
 using InterviewTestPagination.Models.Todo;
@@ -17,7 +18,34 @@ namespace InterviewTestPagination.Controllers {
 
         [HttpGet]
         public IEnumerable<Todo> Todos(/* parameters  */) {
-            return _todoService.Repository.All();
+            return _todoService.List();
+        }
+
+        [HttpGet]
+        public IEnumerable<Todo> Filtered(int page, int page_size, string order_by)
+        {
+
+            var filtered_result = _todoService.List().Skip(page_size * page).Take(page_size);
+
+            switch (order_by)
+            {
+                case "Id":
+                    filtered_result = filtered_result.OrderBy(todo => todo.Id);
+                    break;
+                case "Task":
+                    filtered_result = filtered_result.OrderBy(todo => todo.Task);
+                    break;
+                case "CreatedDate":
+                    filtered_result = filtered_result.OrderBy(todo => todo.CreatedDate);
+                    break;
+                default:
+                    filtered_result = filtered_result.OrderBy(todo => todo.Id);
+                    break;
+
+            }
+
+            return filtered_result;
+
         }
 
     }
